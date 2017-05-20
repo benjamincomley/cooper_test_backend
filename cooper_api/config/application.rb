30c01d2a11896b5f9c21e2fcf16319f9256ea9a1
Benji <benjamincomley@gmail.com>
2017-05-20 18:03:54 +0200

    complete backend

---
+++ cooper_api/config/application.rb
@@ -0,0 +1,26 @@
+require_relative 'boot'
+
+require "rails"
+
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+
+Bundler.require(*Rails.groups)
+
+module CooperApi
+  class Application < Rails::Application
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options, :head],
+                      expose: %w(access-token expiry token-type uid client),
+                      max_age: 0
+      end
+    end
+  end
+end
